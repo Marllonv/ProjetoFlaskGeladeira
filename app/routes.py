988533093cde6e97b13870c34b94f166b54dfef4
Marllonv <marllonvicente20@gmail.com>
2024-10-21 17:50:02 -0300

    commit: att

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,13 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, flash
+import pandas as pd
+
+df = pd.DataFrame({
+    "Produto": ["Leite", "Queijo", "Maçã", "Carne bovina"],
+    "Categoria": ["Laticínios", "Laticínios", "Frutas", "Carnes"],
+    "Data de Validade": ["2024-12-31", "2024-11-20", "2024-11-15", "2024-11-10"],
+    "Quantidade": ["1 litro", "200g", "5 unidades", "500g"]
+})
 
 @app.route('/')
 @app.route('/index')
@@ -8,8 +16,23 @@ def index():
 
 @app.route('/geladeira')
 def geladeira():
-    return render_template('geladeira.html')        
+    return render_template('geladeira.html')
+        
+@app.route('/itens')
+def verItens():
+    return render_template('geladeira.html',  tables=[df.to_html(classes='data')], titles=df.columns.values, tabela=True)
 
 @app.route('/usuario')
 def usuario():
-    return render_template('usuario.html')
+    return render_template('usuario.html', logado=False)
+
+@app.route('/autenticar', methods=['POST'])
+def autenticar():
+    usuario = request.form.get('usuario')
+    senha = request.form.get('senha')
+    if usuario == 'admin' and senha=='admin':
+        return render_template('usuario.html', logado=True)
+    else:
+        flash("Dados Inválidos")
+        return redirect('/usuario')
+
