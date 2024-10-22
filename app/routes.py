d55926424c90dc5074cd9035bf2503ef7cbb5788
Marllonv <marllonvicente20@gmail.com>
2024-10-21 21:14:09 -0300

    Commit: Ultimo Commit Rapeize

--- app/routes.py
+++ app/routes.py
@@ -1,26 +1,36 @@
 from app import app
-from flask import render_template, request, redirect, flash
+from flask import render_template, request, redirect, flash, url_for
 import pandas as pd
 
 df = pd.DataFrame({
-    "Produto": ["Leite", "Queijo", "Maçã", "Carne bovina"],
-    "Categoria": ["Laticínios", "Laticínios", "Frutas", "Carnes"],
-    "Data de Validade": ["2024-12-31", "2024-11-20", "2024-11-15", "2024-11-10"],
-    "Quantidade": ["1 litro", "200g", "5 unidades", "500g"]
+    "Produto": ["Leite", "Queijo", "Maçã", "Carne bovina", "Banana", "Pão", "Tomate", "Ovos", "Arroz"],
+    "Categoria": ["Laticínios", "Laticínios", "Frutas", "Carnes", "Frutas", "Panificação", "Hortifrúti", "Alimentos", "Grãos"],
+    "Data de Validade": ["2024-12-31", "2024-11-20", "2024-11-15", "2024-11-10", "2024-12-10", "2024-11-25", "2024-11-18", "2024-11-12", "2025-01-15"],
+    "Quantidade": ["1 litro", "200g", "5 unidades", "500g", "1 dúzia", "500g", "2kg", "1 cartela", "5kg"]
+})
+
+df2 = pd.DataFrame({
+    "Nome Cartao": [],
+    "Num. Cartao": [],
+    "Seg. Cartao": [],
+    "Val. Cartao": []
 })
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', nome="Maras")
+    return redirect('/usuario')
 
-@app.route('/geladeira')
-def geladeira():
-    return render_template('geladeira.html')
-        
-@app.route('/itens')
-def verItens():
-    return render_template('geladeira.html',  tables=[df.to_html(classes='data')], titles=df.columns.values, tabela=True)
+@app.route('/geladeira', defaults={"temperatura":10})
+@app.route('/geladeira/<int:temperatura>')
+def geladeira(temperatura):
+    return render_template('geladeira.html', temperatura=temperatura)
+    
+
+@app.route('/itens', defaults={"temperatura":10})
+@app.route('/itens/<int:temperatura>')
+def verItens(temperatura):
+    return render_template('geladeira.html', tables=[df.to_html(classes='data')], titles=df.columns.values, tabela=True, temperatura=temperatura)
 
 @app.route('/usuario')
 def usuario():
@@ -35,4 +45,18 @@ def autenticar():
     else:
         flash("Dados Inválidos")
         return redirect('/usuario')
-
+    
+@app.route('/salvar-cartao', methods=['POST'])
+def salvarCartao():
+    cardName = request.form.get('card-name')
+    cardNum = request.form.get('card-num')
+    cardCode = request.form.get('card-code')
+    cardDate = request.form.get('card-date')
+    newDf2 = pd.DataFrame({
+        "Nome Cartao": [cardName],
+        "Num. Cartao": [cardNum],
+        "Seg. Cartao": [cardCode],
+        "Val. Cartao": [cardDate]
+    })
+    
+    return render_template('usuario.html', tables=[newDf2.to_html(classes='data')], titles=df.columns.values, tabela=True, logado=True)
